4b118c8a437b111fae1fdc86be2a5593c75d9cf6
awang1 <awang1@andrew.cmu.edu>
2015-10-07 21:48:23 -0400

    Change placeholder image to one with the right size, from the web

--- client/projectsList/projectCard/projectCard.js
+++ client/projectsList/projectCard/projectCard.js
@@ -1,6 +1,6 @@
 Template.projectCard.helpers({
   imageUrl: function(project) {
-    let placeholder = 'images/placeholder.png';
+    let placeholder = 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=300';
 
     if(!project.cardImage || !project.cardImage.url) {
       return placeholder;
