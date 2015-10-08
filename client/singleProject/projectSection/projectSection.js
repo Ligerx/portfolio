d59a7a1c4d9a1d18b4be184f95fcfa7777e1fc82
awang1 <awang1@andrew.cmu.edu>
2015-10-08 13:33:58 -0400

    Update image template to handle no images

--- client/singleProject/projectSection/projectSection.js
+++ client/singleProject/projectSection/projectSection.js
@@ -1,4 +1,8 @@
 Template.imageTemplate.helpers({
+  noImages: function(images) {
+    return (images.length === 0) ? true : false;
+  },
+
   onlyOneImage: function(images) {
     return (images.length === 1) ? true : false;
   }
